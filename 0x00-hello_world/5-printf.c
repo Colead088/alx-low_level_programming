59bdfb23ea98275bb5751d7caabb59b829fce199
Colead088 <christopher.olasupo@gmail.com>
2023-02-16 19:58:12 -0800

    Others

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * Writing 
+ * a printf function obtained from the stdio library
+ * and returns the value 0
+ * when successful
+ */
+int main(void)
+{
+     printf("with proper grammar, but the outcome is a piece of art,");
+     return (0);
+}
