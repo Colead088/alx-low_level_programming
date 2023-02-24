ec1129160500991db44240b461b3d7cac58495e2
Colead088 <christopher.olasupo@gmail.com>
2023-02-24 01:12:48 -0800

    Nested functions problemSolutions

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int _isupper(int c);
+int _putchar(char c);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_integer(int m);
+void print_number(int n);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+int largest_number(int, int, int);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+#endif /* MAIN_H */
